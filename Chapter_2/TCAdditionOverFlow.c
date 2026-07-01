ad194f6a21da659b5eca9a615c79df5060f8678b
aunghlapaing <aunghlapaing8949@gmail.com>
2026-07-02 00:01:31 +0700

    bugs fixed in Unsigned and Two Complement Addition Overflow case

--- Chapter_2/TCAdditionOverFlow.c
+++ Chapter_2/TCAdditionOverFlow.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int tadd_ok(int x, int y)
-{
-	unsigned num = 4294967295u;
-	return -2147483648 <= x+y && x+y < 2147483648;
+{	
+	int sum = x + y;
+	return !( x > 0 && y > 0 && sum <= 0 ) && !( x < 0 && y < 0 && sum >= 0 ) ;
 }
 
 int main()
 {
 
-	int x = -2147483648;
+	int x = 2147483647;
 	int y = 0;
 	printf( "Two Complement Addition : %d\n", tadd_ok(x, y) );
 	return 0;
